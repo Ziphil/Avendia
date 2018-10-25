c6ec05d7dfa0effce6a1dc3e29cd251b10774d04
Ziphil <yfe23457@nifty.com>
2018-10-25 18:40:00 +0900

    HTML生成規則の指定方式を変更

--- convert.rb
+++ convert.rb
@@ -2,6 +2,7 @@
 
 
 require 'pp'
+require 'fileutils'
 require 'rexml/document'
 include REXML
 
@@ -60,351 +61,30 @@ class ZiphilConverter
     @main_string = ""
     @title = ""
     @description = ""
+    @templates = {}
   end
 
   def convert
     source = File.read(@path)
     element = Document.new(source, {:raw => :all}).root
-    convert_element(element)
+    convert_element(element, "")
   end
 
-  def convert_element(element)
-    case element.name
-    when "page"
-      deepness = WholeZiphilConverter.deepness(@path, @language)
-      virtual_deepness = (@path =~ /index\.zpml$/) ? deepness - 1 : deepness
-      name_tag = TagBuilder.new("div", "name")
-      if virtual_deepness >= -1
-        first_category = @path.split("/")[-deepness - 1]
-        first_link_tag = TagBuilder.new("a", "name")
-        first_link_tag["href"] = "../../" + first_category
-        if virtual_deepness == -1
-          first_link_tag << NAMES[:top][@language]
-        else
-          first_link_tag << NAMES[first_category.intern][@language]
-        end
-        name_tag << first_link_tag
+  def convert_element(element, scope)
+    @templates.each do |(element_pattern, scope_pattern), block|
+      if element_pattern === element.name && scope_pattern === scope
+        return block.call(element)
       end
-      if virtual_deepness >= 1
-        second_category = @path.split("/")[-deepness]
-        united_category = first_category + "_" + second_category
-        second_link_tag = TagBuilder.new("a", "name")
-        second_link_tag["href"] = "../" + second_category
-        second_link_tag << NAMES[united_category.intern][@language]
-        name_tag << second_link_tag
-      end
-      if virtual_deepness >= 2
-        if element.attribute("link")
-          converted_path = @path.match(/([0-9a-z\-]+)\.zpml/).to_a[1].to_s
-          converted_path += (element.attribute("link").value == "c") ? ".cgi" : ".html"
-          third_link_tag = TagBuilder.new("a", "name")
-          third_link_tag["href"] = converted_path
-        else
-          third_link_tag = TagBuilder.new("span")
-        end
-        name_element = element.elements["name"]
-        third_link_tag << convert_children(name_element)
-        name_tag << third_link_tag
-        @title = flatten_text(name_element.inner_text(true))
-      end
-      @navigation_string << name_tag
-      @main_string << convert_children(element)
-    when "name"
-    when "ver"
-      if element.text == "*" || element.text =~ LATEST_VERSION_REGEX
-        version_tag = TagBuilder.new("div", "version")
-        @latest = true
-      else
-        version_tag = TagBuilder.new("div", "version-caution")
-      end
-      version_tag << convert_children(element)
-      @navigation_string << version_tag
-    when "import-script"
-      script_tag = TagBuilder.new("script")
-      script_tag["src"] = self.url_prefix + "file/script/" + element.attribute("src").to_s
-      @header_string << script_tag
-      @header_string << "\n"
-    when "base"
-      base_tag = TagBuilder.new("base")
-      base_tag["href"] = element.attribute("href").to_s
-      @header_string << base_tag
-      @header_string << "\n"
-    when "pb"
-      tag = TagBuilder.new("div", "index-container")
-      tag << convert_children(element)
-    when "hb"
-      tag = TagBuilder.new("div", "index-header")
-      tag << convert_children(element)
-    when "ab", "abo", "aba", "abd"
-      case element.name
-      when "ab"
-        tag = TagBuilder.new("a", "index")
-      when "abo"
-        tag = TagBuilder.new("a", "old-index")
-      when "aba"
-        tag = TagBuilder.new("a", "ancient-index")
-      when "abd"
-        tag = TagBuilder.new("div", "disabled-index")
-      end
-      element.attributes.each_attribute do |attribute|
-        if attribute.name == "date"
-          date_tag = TagBuilder.new("span", "date")
-          if element.attribute("date").value =~ /^\d+$/
-            hairia_tag = TagBuilder.new("span", "hairia")
-            hairia_tag << element.attribute("date").to_s
-            date_tag << hairia_tag
-          else
-            date_tag << element.attribute("date").to_s
-          end
-          tag << date_tag
-        else
-          tag[attribute.name] = attribute.to_s
-        end
-      end
-      content_tag = TagBuilder.new("span", "content")
-      content_tag << convert_children(element)
-      tag << content_tag
-    when "h1", "h2"
-      tag = TagBuilder.new(element.name)
-      element.attributes.each_attribute do |attribute|
-        if attribute.name == "number"
-          number_tag = TagBuilder.new("span", "number")
-          number_tag << element.attribute("number").to_s
-          tag["id"] = element.attribute("number").to_s
-          tag << number_tag
-        else
-          tag[attribute.name] = attribute.to_s
-        end
-      end
-      tag << convert_children(element)
-    when "p"
-      tag = TagBuilder.new("p")
-      tag << convert_children(element)
-      if element.attribute("par")
-        additional_tag = TagBuilder.new("span", "paragraph")
-        additional_tag << element.attribute("par").to_s
-        tag.insert_first(additional_tag)
-      end
-      if element.attribute("name")
-        additional_tag = TagBuilder.new("span", "name")
-        additional_tag << element.attribute("name").to_s
-        tag.insert_first(additional_tag)
-      end
-      @description << flatten_text(element.inner_text(true))
-    when "red"
-      tag = TagBuilder.new("span", "redact")
-      tag << "&nbsp;" * element.attribute("length").to_s.to_i
-    when "img"
-      tag = TagBuilder.new("img", nil, false)
-      tag["alt"] = ""
-      element.attributes.each_attribute do |attribute|
-        tag[attribute.name] = attribute.to_s
-      end
-      tag << convert_children(element)
-    when "a"
-      tag = pass_element(element)
-    when "an"
-      tag = TagBuilder.new("a", "normal")
-      element.attributes.each_attribute do |attribute|
-        tag[attribute.name] = attribute.to_s
-      end
-      tag << convert_children(element)
-    when "ol", "ul"
-      tag = pass_element(element)
-    when "xl"
-      tag = TagBuilder.new("ul", "conlang")
-      tag << convert_children(element) do |inner_element|
-        case inner_element.name
-        when "li"
-          inner_tag = TagBuilder.new("li")
-          inner_tag << convert_children(inner_element) do |profound_element|
-            case profound_element.name
-            when "sh"
-              profound_tag = TagBuilder.new
-              profound_tag << convert_children(profound_element)
-            when "ja"
-              profound_tag = TagBuilder.new("ul")
-              profound_item_tag = TagBuilder.new("li")
-              profound_item_tag << convert_children(profound_element)
-              profound_tag << profound_item_tag
-            end
-            next profound_tag
-          end
-        end
-        next inner_tag
-      end
-    when "el"
-      tag = TagBuilder.new("table", "list")
-      tag << convert_children(element) do |inner_element|
-        case inner_element.name
-        when "li"
-          inner_tag = TagBuilder.new("tr")
-          inner_tag << convert_children(inner_element) do |profound_element|
-            case profound_element.name
-            when "et", "ed"
-              profound_tag = TagBuilder.new("td")
-              profound_tag << convert_children(profound_element)
-            end
-            next profound_tag
-          end
-        end
-        next inner_tag
-      end
-    when "trans"
-      tag = TagBuilder.new("table", "translation")
-      tag << convert_children(element) do |inner_element|
-        case inner_element.name
-        when "li"
-          inner_tag = TagBuilder.new("tr")
-          inner_tag << convert_children(inner_element) do |profound_element|
-            case profound_element.name
-            when "ja", "sh"
-              profound_tag = TagBuilder.new("td")
-              profound_tag << convert_children(profound_element)
-            end
-            next profound_tag
-          end
-        end
-        next inner_tag
-      end
-    when "section-table"
-      tag = TagBuilder.new("ul", "section-table")
-      section_item_tag = TagBuilder.new("li")
-      subsection_tag = TagBuilder.new("ul")
-      element.each_xpath("/page/*[name() = 'h1' or name() = 'h2']") do |inner_element|
-        case inner_element.name
-        when "h1"
-          unless section_item_tag.content.empty?
-            section_item_tag << subsection_tag unless subsection_tag.content.empty?
-            tag << section_item_tag
-          end
-          section_item_tag = TagBuilder.new("li")
-          subsection_tag = TagBuilder.new("ul")
-          section_item_tag << convert_text(inner_element.get_text.to_s)
-        when "h2"
-          subsection_item_tag = TagBuilder.new("li")
-          if inner_element.attribute("number")
-            number_tag = TagBuilder.new("span", "number")
-            link_tag = TagBuilder.new("a")
-            number_tag << inner_element.attribute("number").to_s
-            link_tag["href"] = "#" + inner_element.attribute("number").to_s
-            link_tag << convert_text(inner_element.get_text.to_s.gsub(/\{(.+?)\}/){"\[#{$1}\]"})
-            subsection_item_tag << number_tag
-            subsection_item_tag << link_tag
-          else
-            subsection_item_tag << convert_text(inner_element.get_text.to_s)
-          end
-          subsection_tag << subsection_item_tag
-        end
-      end
-      section_item_tag << subsection_tag unless subsection_tag.content.empty?
-      tag << section_item_tag
-    when "li"
-      tag = pass_element(element)
-    when "table"
-      tag = pass_element(element)
-    when "tr", "th", "td"
-      tag = pass_element(element)
-    when "thl"
-      tag = TagBuilder.new("th", "left")
-      tag << convert_children(element)
-    when "form"
-      tag = pass_element(element)
-    when "input"
-      tag = pass_element(element, false)
-    when "textarea"
-      tag = pass_element(element)
-    when "error"
-      tag = TagBuilder.new("div", "error")
-      tag << convert_children(element) do |inner_element|
-        case inner_element.name
-        when "code"
-          inner_tag = TagBuilder.new("div", "error-code")
-          inner_tag << convert_children(inner_element)
-        when "message"
-          inner_tag = TagBuilder.new("div", "message")
-          inner_tag << convert_children(inner_element)
-        end
-        next inner_tag
-      end
-    when "pdf"
-      tag = TagBuilder.new("object", "pdf")
-      tag["data"] = element.attribute("src").to_s + "#view=FitH&amp;statusbar=0&amp;toolbar=0&amp;navpanes=0&amp;messages=0"
-      tag["type"] = "application/pdf"
-    when "slide"
-      tag = TagBuilder.new("div", "slide")
-      script_tag = TagBuilder.new("script", "speakerdeck-embed")
-      script_tag["async"] = "async"
-      script_tag["data-id"] = element.attribute("id").to_s
-      script_tag["data-ratio"] = "1.33333333333333"
-      script_tag["src"] = "//speakerdeck.com/assets/embed.js"
-      tag << script_tag
-    when "pre", "samp"
-      case element.name
-      when "pre"
-        tag = TagBuilder.new("table", "code")
-      when "samp"
-        tag = TagBuilder.new("table", "sample")
-      end
-      text = element.get_text.to_s.gsub(/\A\s*?\n/, "")
-      indent_size = text.match(/\A(\s*?)\S/)[1].length
-      text = text.rstrip.deindent
-      tag << "\n"
-      text.each_line do |line|
-        row_tag = TagBuilder.new("tr")
-        code_tag = TagBuilder.new("td")
-        code_tag << line.chomp
-        row_tag << code_tag
-        tag << " " * indent_size
-        tag << row_tag
-        tag << "\n"
-      end
-      tag << " " * (indent_size - 2)
-    when "c", "m"
-      case element.name
-      when "c"
-        tag = TagBuilder.new("span", "code")
-      when "m"
-        tag = TagBuilder.new("span", "monospace")
-      end
-      element.children.each do |inner_element|
-        case inner_element
-        when Element
-          tag << convert_element(inner_element)
-        when Text
-          tag << inner_element.to_s
-        end
-      end
-    when "special"
-      tag = pass_element(element)
-    when "sup", "sub"
-      tag = pass_element(element)
-    when "h"
-      tag = TagBuilder.new("span", "hairia")
-      tag << convert_children(element)
-    when "k"
-      tag = TagBuilder.new("span", "japanese")
-      tag << convert_children(element)
-    when "i"
-      tag = pass_element(element)
-    when "fl"
-      tag = TagBuilder.new("span", "foreign")
-      tag << convert_children(element)
-    when "small"
-      tag = TagBuilder.new("span", "small")
-      tag << convert_children(element)
-    when "br"
-      tag = pass_element(element, false)
     end
-    return tag
+    return ""
   end
 
-  def pass_element(element, close = true)
+  def pass_element(element, scope, close = true)
     tag = TagBuilder.new(element.name, nil, close)
     element.attributes.each_attribute do |attribute|
       tag[attribute.name] = attribute.to_s
     end
-    tag << convert_children(element)
+    tag << apply(element, scope)
     return tag
   end
 
@@ -484,7 +164,7 @@ class ZiphilConverter
     return text
   end
 
-  def convert_children(element, &block)
+  def apply(element, scope, &block)
     result = ""
     element.children.each do |inner_element|
       case inner_element
@@ -495,7 +175,7 @@ class ZiphilConverter
             result << tag
           end
         else
-          tag = convert_element(inner_element)
+          tag = convert_element(inner_element, scope)
           if tag
             result << tag
           end
@@ -507,10 +187,15 @@ class ZiphilConverter
     return result
   end
 
+  def add(element_pattern, scope_pattern, &block)
+    @templates.store([element_pattern, scope_pattern], block)
+  end
+
   def save
-    convert
-    File.open(@path.gsub("_source", "").gsub(".zpml", ".html"), "w") do |file|
-      output = ORIGINAL_DATA.gsub(/#\{(.*?)\}\n?/){eval($1)}.gsub(/\r/, "")
+    path = @path.gsub("_source", "").gsub(".zpml", ".html")
+    FileUtils.mkdir_p(File.dirname(path))
+    File.open(path, "w") do |file|
+      output = ORIGINAL_DATA.gsub(/#\{(.*?)\}/){self.instance_eval($1)}.gsub(/\r/, "")
       file.print(output)
     end
   end
@@ -553,6 +238,8 @@ class WholeZiphilConverter
     paths.each_with_index do |(path, language), index|
       before_time = Time.now
       converter = ZiphilConverter.new(path, language)
+      converter.instance_eval(File.read(File.dirname($0) + "/template.rb"))
+      converter.convert
       converter.save
       after_time = Time.now
       output = " "
@@ -726,6 +413,15 @@ class String
   def deindent!
     margin = self.scan(/^ +/).map(&:size).min
     self.gsub!(/^ {#{margin}}/, "")
+  end
+
+end
+
+
+class Array
+
+  def ===(other)
+    return self.include?(other)
   end
 
 end
